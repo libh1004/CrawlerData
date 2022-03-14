34314481b82750c694e86e003d61e7cc204eb15f
chinhdtth2009021 <chinhdtth2009021@fpt.edu.vn>
2022-03-14 21:43:24 +0700

    CRUD

--- CrawlerDataProject/Areas/Admin/Controllers/AccountController.cs
+++ CrawlerDataProject/Areas/Admin/Controllers/AccountController.cs
@@ -1,19 +1,39 @@
 ﻿using CrawlerDataProject.Data;
+using CrawlerDataProject.Models;
+using CrawlerDataProject.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace CrawlerDataProject.Controllers
+namespace CrawlerDataProject.Areas.Admin.Controllers
 {
     public class AccountController : Controller
     {
         MyDbContext db = new MyDbContext();
+        [HttpGet]
         public ActionResult Index()
         {
             return View(db.Accounts.ToList());
         }
-        public 
+        [HttpGet] //truyền  vào 
+        public ActionResult Create(){
+            return View();
+        }
+        [HttpPost] // Gui di
+        public ActionResult Create(AccountViewModel acc)
+        {
+            Account account = new Account()
+            {
+                Fullname = acc.Fullname,
+                Password = acc.Password,
+                Email = acc.Email,
+                Phone = acc.Phone 
+            };
+            db.Accounts.Add(account); //them vao db 
+            db.SaveChanges(); //luu vao db 
+            return RedirectToAction("Index"); // quay ve va show ra danh sach 
+        }
     }
 }
